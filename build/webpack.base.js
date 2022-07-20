76040217107a10732b638d664bf51971d3520f33
rylanwe <2459493196@qq.com>
2022-07-20 21:01:13 +0800

    chore: init

---
+++ build/webpack.base.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const chalk = require('chalk');
+
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/chunk-[contenthash].js',
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      // js 在 body 里引入
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/chunk-[contenthash].css',
+      ignoreOrder: true
+    }),
+    new VueLoaderPlugin(),
+    new ProgressBarPlugin({
+      format: ` build [:bar] ${chalk.green.bold(':percent')} (:elapsed seconds)`,
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.((c|sc)ss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/,
+        parser: {
+          dataUrlCondition: {
+            maxsize: 25 * 1024,
+          }
+        },
+        generator: {
+          filename: 'images/[contenthash][ext][query]',
+        },
+      },
+      {
+        test: /.\js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+    },
+    extensions: ['.js', '.ts', '.less', '.vue']
+  },
+  devServer: {
+    port: 8080,
+    open: true
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    Hello, world!
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.container {
+  width: 500px;
+  height: 300px;
+  color: orange;
+  background-color: #ccc;
+}
+</style>
